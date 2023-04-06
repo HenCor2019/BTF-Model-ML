1c2795db206e9e29660e82d6685a5d244f55f0e3
HenCor2019 <00095119@uca.edu.sv>
2023-04-06 14:53:14 -0600

    :sparkles: Update Matrix

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ BASE_URL = "http://localhost:8501/v1/models/brain_tumor_detection:predict"
 
 request_json = json.dumps({
    "signature_name": "serving_default",
-   "instances": []
+   "instances": [[[[0.1, 1, 3], [0.1, 1, 3]]]]
 })
 
 response = requests.post(BASE_URL, data=request_json)
